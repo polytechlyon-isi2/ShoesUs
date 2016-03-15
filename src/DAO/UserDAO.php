d9184459b8bce41c9dd67ea296c8b76e350c7499
alexiscalvi <alex-goal73@hotmail.fr>
2016-03-15 16:03:48 +0100

    userDAO

---
+++ src/DAO/UserDAO.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ShoesUs\DAO;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use MicroCMS\Domain\User;
+
+class UserDAO extends DAO implements UserProviderInterface
+{
+    /**
+     * Returns a user matching the supplied id.
+     *
+     * @param integer $id The user id.
+     *
+     * @return \ShoesUs\Domain\User|throws an exception if no matching user is found
+     */
+    public function find($id) {
+        $sql = "select * from s_user where user_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No user matching id " . $id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        $sql = "select * from s_user where user_name=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($username));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $class));
+        }
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsClass($class)
+    {
+        return 'ShoesUs\Domain\User' === $class;
+    }
+
+    /**
+     * Creates a User object based on a DB row.
+     *
+     * @param array $row The DB row containing User data.
+     * @return \ShoesUs\Domain\User
+     */
+    protected function buildDomainObject($row) {
+        $user = new User();
+        $user->setId($row['user_id']);
+        $user->setUsername($row['user_name']);
+        $user->setPassword($row['user_password']);
+        $user->setSalt($row['user_salt']);
+        $user->setRole($row['user_role']);
+        return $user;
+    }
+}
