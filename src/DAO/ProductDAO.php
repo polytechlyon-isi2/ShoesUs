de30a619e2fefbe45553919302f4931a39c88cae
Lucas Garin <lucas.garin73@gmail.com>
2016-03-15 15:16:20 +0100

    Ajout de la recherche de tout les article par categorie

--- src/DAO/ProductDAO.php
+++ src/DAO/ProductDAO.php
@@ -5,8 +5,44 @@ namespace ShoesUs\DAO;
 use ShoesUs\Domain\Product;
 
 class ProductDAO extends DAO
-{
+{   
+    /**
+     * @var \MicroCMS\DAO\CategoryDAO
+     */
+    private $categoryDAO;
+    
+    public function setcategoryDAO(CategoryDAO $categoryDAO) {
+        $this->categoryDAO = $categoryDAO;
+    }
 
+        /**
+     * Return a list of all products for a category, sorted by date (most recent last).
+     *
+     * @param integer $categoryID The category id.
+     *
+     * @return array A list of all products for the category.
+     */
+    public function findAllByCategory($categoryID) {
+        // The associated category is retrieved only once
+        $category = $this->categoryDAO->find($categoryID);
+
+        // cat_id is not selected by the SQL query
+        // The catgeory won't be retrieved during domain objet construction
+        $sql = "select prod_id, prod_name, prod_desc, prod_price from s_product where prod_cat=? order by prod_id";
+        $result = $this->getDb()->fetchAll($sql, array($categoryID));
+
+        // Convert query result to an array of domain objects
+        $products = array();
+        foreach ($result as $row) {
+            $productId = $row['prod_id'];
+            $product = $this->buildDomainObject($row);
+            // The associated article is defined for the constructed comment
+            $product->setCategory($category);
+            $products[$productId] = $product;
+        }
+        return $products;
+    }
+    
     /**
      * Return a list of all products, sorted by date (most recent first).
      *
