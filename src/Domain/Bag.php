ab82c857ba393aea4eef12fae96091d390d415c9
alexiscalvi <alex-goal73@hotmail.fr>
2016-03-22 15:10:53 +0100

    add Bag

---
+++ src/Domain/Bag.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ShoesUs\Domain;
+
+class Product 
+{
+    /**
+     * User.
+     *
+     * @var integer
+     */
+    private $user;
+
+    /**
+     * Product.
+     *
+     * @var integer
+     */
+    private $prod;
+    
+
+    public function getUser() {
+        return $this->user;
+    }
+
+    public function setUser($user) {
+        $this->user = $user;
+    }
+
+
+
+
+    public function getProd() {
+        return $this->prod;
+    }
+
+    public function setProd($prod) {
+        $this->prod = $prod;
+    }
+
+}
